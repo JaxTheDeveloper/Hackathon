932a68a0927b9f85dee005010f59747ce1f7b627
Thanh Hoang <50241437+JaxTheDeveloper@users.noreply.github.com>
2021-04-13 20:48:36 +0700

    Update motion_detector.py

--- 26. Application 6  Build a Webcam Motion Detector/1.1 app6_motion_detector/motion_detector.py
+++ 26. Application 6  Build a Webcam Motion Detector/1.1 app6_motion_detector/motion_detector.py
@@ -22,7 +22,7 @@ while True:
     thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
 
-    (_,cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    (cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
         if cv2.contourArea(contour) < 10000:
@@ -54,7 +54,7 @@ while True:
             times.append(datetime.now())
         break
 
-print(status_list)
+print(status_list)  
 print(times)
 
 for i in range(0,len(times),2):

86937f82a7b3b60f84fee5493bd18350f8a295c6
Thanh Hoang <50241437+JaxTheDeveloper@users.noreply.github.com>
2021-04-13 20:49:12 +0700

    Update plotting.py

--- 26. Application 6  Build a Webcam Motion Detector/1.1 app6_motion_detector/plotting.py
+++ 26. Application 6  Build a Webcam Motion Detector/1.1 app6_motion_detector/plotting.py
@@ -10,7 +10,8 @@ cds=ColumnDataSource(df)
 
 p=figure(x_axis_type='datetime',height=100, width=500, sizing_mode = "scale_width", title="Motion Graph")
 p.yaxis.minor_tick_line_color=None
-p.ygrid[0].ticker.desired_num_ticks=1
+# p.ygrid[0].ticker.desired_num_ticks=1
+p.yaxis.ticker.desired_num_ticks = 1
 
 hover=HoverTool(tooltips=[("Start","@Start_string"),("End","@End_string")])
 p.add_tools(hover)

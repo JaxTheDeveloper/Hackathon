8960ed91bdc689a628885325601dd13408f3e1e9
Thanh Hoang <50241437+JaxTheDeveloper@users.noreply.github.com>
2021-04-15 22:21:11 +0700

    Update blink_detect.py

--- blink_detect.py
+++ blink_detect.py
@@ -1,113 +1,126 @@
-#All the imports go here
+# All the imports go here
 import numpy as np
 import cv2
 import time
 import datetime as date
 import logging as log
 import pandas
-from bokeh.plotting import figure, show, output_file
-from bokeh.models import HoverTool, ColumnDataSource
 
-#Initializing the face and eye cascade classifiers from xml files
+# Initializing the face and eye cascade classifiers from xml files
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
 
-#Variable store execution state
+# Variable store execution state
 first_read = True
 
-#Starting the video capture
+# Starting the video capture
 cap = cv2.VideoCapture(0)
 ret, img = cap.read()
-log.basicConfig(filename='app.log', filemode='w',format='%(asctime)s - %(message)s', level=log.INFO)
+log.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(message)s', level=log.INFO)
 log.info('Camera Initialized')
-first_frame=None
+first_frame = None
 
-#variable declaration
+# variable declaration
 blinktimes = 0
 statuslis = [None, None]
 timeevents = []
-df = pandas.DataFrame(columns=['Nface ','Neyes','Affrm'])
-timeevents.append([0,date.datetime.now()])
+nface = []
+neyes = []
+afrm = []
+df = pandas.DataFrame(columns=['Nface', 'Neyes', 'Affrm'])
+timeevents.append([0, date.datetime.now()])
 
-while(ret):
 
-    ret,img = cap.read()
-    #Coverting the recorded image to grayscale
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    gray2=cv2.GaussianBlur(gray,(21,21),0)
-    #delta_frame=cv2.absdiff(ret,gray)
-    #thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
-    #thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
-    #Applying filter to remove impurities
-    gray = cv2.bilateralFilter(gray,5,1,1)
+while ret:
 
-    #Detecting the face for region of image to be fed to eye classifier
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5,minSize=(200,200))
-    if(len(faces)>0):
+    ret, img = cap.read()
+    # Converting the recorded image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.GaussianBlur(gray, (21, 21), 0)
+    # delta_frame=cv2.absdiff(ret,gray)
+    # thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    # thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
+    # Applying filter to remove impurities
+    gray = cv2.bilateralFilter(gray, 5, 1, 1)
+
+    # Detecting the face for region of image to be fed to eye classifier
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(200, 200))
+    if (len(faces) > 0):
         timeelasped = time.time()
-        for (x,y,w,h) in faces:
-            img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        for (x, y, w, h) in faces:
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-            #roi_face is face which is input to eye classifier
-            roi_face = gray[y:y+h,x:x+w]
-            roi_face_clr = img[y:y+h,x:x+w]
-            eyes = eye_cascade.detectMultiScale(roi_face,1.3,5,minSize=(50,50))
+            # roi_face is face which is input to eye classifier
+            roi_face = gray[y:y + h, x:x + w]
+            roi_face_clr = img[y:y + h, x:x + w]
+            eyes = eye_cascade.detectMultiScale(roi_face, 1.3, 5, minSize=(50, 50))
 
-            #Examining the length of eyes object for eyes
-            if(len(eyes)>=2):
-                #Check if program is running for detection 
-                if(first_read):
-                    cv2.putText(img, "Eye detected", (70,70), cv2.FONT_HERSHEY_PLAIN, 3,(0,255,0),2)
+            # Examining the length of eyes object for eyes
+            if (len(eyes) >= 2):
+                # Check if program is running for detection
+                if (first_read):
+                    cv2.putText(img, "Eye detected", (70, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
                     log.info("eyes detected " + str(len(eyes)) + " at " + str(date.datetime.now()))
                     status = 2
                     statuslis.append(status)
-                    timeevents.append([2,date.datetime.now()])
+                    timeevents.append([2, date.datetime.now()])
 
-                #else:
+                # else:
                 #    cv2.putText(img, "Eyes open!", (70,70), cv2.FONT_HERSHEY_PLAIN, 2,(255,255,255),2)
                 #    log.info("eyes open " + str(len(eyes)) + " at " + str(date.datetime.now()))
             else:
 
-                if(first_read):
-                    #To ensure if the eyes are present before starting
-                    cv2.putText(img, "No eyes detected", (70,70), cv2.FONT_HERSHEY_PLAIN, 3,(0,0,255),2)
+                if (first_read):
+                    # To ensure if the eyes are present before starting
+                    cv2.putText(img, "No eyes detected", (70, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 2)
                     log.info("unindentified eyes" + " at " + str(date.datetime.now()))
                     status = 1
                     statuslis.append(status)
                     timeevents.append([1, date.datetime.now()])
-                #else:
-                    ##This will print on console and restart the algorithm
-                    #print("Blink detected--------------")
-                    #cv2.waitKey(3000)
-                    #first_read=True
+                # else:
+                ##This will print on console and restart the algorithm
+                # print("Blink detected--------------")
+                # cv2.waitKey(3000)
+                # first_read=True
 
     else:
-        cv2.putText(img,"No face detected",(100,100),cv2.FONT_HERSHEY_PLAIN, 3, (0,255,0),2)
+        cv2.putText(img, "No face detected", (100, 100), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
         status = 0
         statuslis.append(status)
         timeevents.append([0, date.datetime.now()])
 
-    #Controlling the algorithm with keys
-    cv2.imshow('img',img)
+    # Controlling the algorithm with keys
+    cv2.imshow('img', img)
     a = cv2.waitKey(1)
 
     print(status)
-    if(a==ord('q')):
+    if (a == ord('q')):
         print(blinktimes)
         print(statuslis)
         print(timeevents)
         if status != 0:
-            timeevents.append[0,date.datetime.now()]
-        #timeevents.append[[0],['anta ga baka']]
+            timeevents.append([0, date.datetime.now()])
         break
-    elif(a==ord('s') and first_read):
-        #This will start the detection
+    elif (a == ord('s') and first_read):
+        # This will start the detection
         first_read = False
 
-for i in range(0,len(timeevents)):
-    #df=df.append({"Start":str(timeevents[i][0]),"End":timeevents[i]},ignore_index=True)
+#for i in range(0, len(timeevents)):
+#    if timeevents[i][0] == 0: nface.append(timeevents[i][1])
+#    elif timeevents[i][0] == 1: neyes.append(timeevents[i][1])
+#    elif timeevents[i][0] == 2: nface.append(timeevents[i][1])
+
+
+for i in range(0, len(timeevents)):
+    if i == 0:
+        if timeevents[i][0] == 0:  df = df.append({"NfaceStart": str(timeevents[i][1])}, ignore_index=True)
+        elif timeevents[i][0] == 1:  df = df.append({"NyeysStart": str(timeevents[i][1])}, ignore_index=True)
+        elif timeevents[i][0] == 2:  df = df.append({"Affrmtart": str(timeevents[i][1])}, ignore_index=True)
+
+
+    # df=df.append({"Start":str(timeevents[i][0]),"End":timeevents[i]},ignore_index=True)
     if timeevents[i][0] == 0:
-        df = df.append({"Nface":str(timeevents[i][1])}, ignore_index=True)
+        df = df.append({"Nface": str(timeevents[i][1])}, ignore_index=True)
     elif timeevents[i][0] == 1:
         df = df.append({"Neyes": str(timeevents[i][1])}, ignore_index=True)
     elif timeevents[i][0] == 2:
